9414a7835a734e1aca622c56ee26fdc2559c58f5
sara nicolay <nicolay.sara@gmail.com>
2020-02-19 14:29:22 +0100

    Done

---
+++ db/migrate/20200219111154_change_name_column_phone_number.rb
@@ -0,0 +1,5 @@
+class ChangeNameColumnPhoneNumber < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :restaurants, :phonenumber, :phone_number
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,11 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+
+# Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create([{ name: 'resto1' }, { adress: 'BX' }, { phone_number: '+32 222' }, { category: 'chinese' }])
+Restaurant.create([{ name: 'resto2' }, { adress: 'ARLON' }, { phone_number: '+32 223' }, { category: 'italian' }])
+Restaurant.create([{ name: 'resto3' }, { adress: 'LIEGE' }, { phone_number: '+32 224' }, { category: 'japanese' }])
+Restaurant.create([{ name: 'resto4' }, { adress: 'ANVERS' }, { phone_number: '+32 225' }, { category: 'french' }])
+Restaurant.create([{ name: 'resto5' }, { adress: 'DAKAR' }, { phone_number: '+32 226' }, { category: 'belgian' }])
